2955d0c96f52072cacd5be0d05b0c39ea10f770d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-19 16:24:32 -0300

    feat(verifyUserBody.ts) cria middleware que verifica corpo da requisicao

---
+++ src/middlewares/user/verifyUserBody.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { userSchema } from "../../schemas";
+import { handleError } from "../../utils";
+
+const verifyUserBody = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await userSchema.validate(req.body);
+
+    next();
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+};
+
+export default verifyUserBody;
