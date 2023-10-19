cbe513fdddb3a51c1221743a02674a12d895cafb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-19 16:24:00 -0300

    feat(connection.ts) adiciona configuração de conexão com o banco de dados

---
+++ src/data/connection.ts
@@ -0,0 +1,13 @@
+import knex from "knex";
+
+const db = knex({
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATA,
+  }
+});
+
+export default db;
