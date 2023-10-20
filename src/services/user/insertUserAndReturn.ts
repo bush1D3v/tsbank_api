12c927e8ff0ed59f18ac3bdc33d1c832ae21cd25
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-20 11:55:52 -0300

    fix(insertUserAndReturn.ts) adicionando verificacao de existencia de email no banco de dados

--- src/services/user/insertUserAndReturn.ts
+++ src/services/user/insertUserAndReturn.ts
@@ -1,9 +1,15 @@
 import { UserParams } from "../../models";
-import { createNewUser } from "../../repositories";
+import { createNewUser, validateEmail } from "../../repositories";
 import { encryptPassword } from "../../utils";
 
 const insertUserAndReturn = async (params: UserParams) => {
   const cryptPassword = await encryptPassword(params.password);
+
+  const emailExists = await validateEmail(params.email);
+
+  if (emailExists) {
+    throw new Error("Email already exists");
+  }
 
   const newUser = [
     params.name,
