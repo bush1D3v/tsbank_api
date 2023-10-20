fa34138620e60a8ae07ef03fbae5e7c1683bdc41
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-20 12:09:26 -0300

    feat(verifyLoginUserBody.ts) cria middleware verificador do corpo da requisicao da rota de login do usuario

---
+++ src/middlewares/user/verifyLoginUserBody.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { handleError } from "../../utils";
+import { loginUserSchema } from "../../schemas";
+
+const verifyLoginUserBody = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await loginUserSchema.validate(req.body);
+
+    next();
+  } catch (error: any) {
+    handleError(res, error, 401);
+  }
+};
+
+export default verifyLoginUserBody;
