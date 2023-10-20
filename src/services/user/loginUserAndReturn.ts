ef52d6eb76026fada2805651a8b3afb63c6bbe6e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-20 12:15:40 -0300

    feat(loginUserAndReturn.ts) cria servico que puxa usuario no banco de dados comparando a senha nao encriptada com a encriptada e retorna alguns dados como resposta da requisicao, alem do token do usuario

---
+++ src/services/user/loginUserAndReturn.ts
@@ -0,0 +1,22 @@
+import { validateEmail, validatePassword } from "../../repositories";
+import { createToken } from "../../utils";
+
+const loginUserAndReturn = async (email: string, password: string) => {
+  const user = await validateEmail(email);
+
+  if (typeof user === "undefined") {
+    throw new Error("Invalid username and/or password");
+  }
+
+  const validPassword = await validatePassword(password, user.password);
+
+  if (!validPassword) {
+    throw new Error("Invalid username and/or password");
+  }
+
+  const response = createToken(user);
+
+  return response;
+};
+
+export default loginUserAndReturn;
