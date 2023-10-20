d3b271fbe5703c76ee1f5ea21e2f1d0cf15cc7cb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-20 13:04:16 -0300

    update(index.ts) exportando todas as utilidades

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import handleError from "./handleError";
 import encryptPassword from "./encryptPassword";
+import createToken from "./createToken";
 
-export { handleError, encryptPassword };
+export { handleError, encryptPassword, createToken };
