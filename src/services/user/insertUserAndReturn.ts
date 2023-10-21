b1ac5afe44d0aade693890b378ae5d7f91fe06d0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-20 23:31:29 -0300

    refactor(insertUserAndReturn.ts) removendo verificação de email de dentro do servico

--- src/services/user/insertUserAndReturn.ts
+++ src/services/user/insertUserAndReturn.ts
@@ -1,15 +1,9 @@
 import { UserParams } from "../../models";
-import { createNewUser, validateEmail } from "../../repositories";
+import { createNewUser } from "../../repositories";
 import { encryptPassword } from "../../utils";
 
 const insertUserAndReturn = async (params: UserParams) => {
   const cryptPassword = await encryptPassword(params.password);
-
-  const emailExists = await validateEmail(params.email);
-
-  if (emailExists) {
-    throw new Error("Email already exists");
-  }
 
   const newUser = [
     params.name,
