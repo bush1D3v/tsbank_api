aa92a5f47e0df04ae2d3db3ade6a4cfce2383032
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-22 00:30:51 -0300

    test(insertUser.tets.ts) adiciona teste para rota de insercao de usuario

---
+++ tests/post/user/insertUser.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import server from "../../../src/server";
+
+describe("User Insertion Controller Tests", () => {
+  it("Must create a new user successfully", async () => {
+    const newUser = {
+      name: "New User",
+      email: "new@user.com",
+      password: "password123",
+    };
+
+    const response = await request(server)
+      .post("/user")
+      .send(newUser);
+
+    expect(response.status).toBe(201);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("name", newUser.name);
+    expect(response.body).toHaveProperty("email", newUser.email);
+  });
+
+  it("Must handle errors correctly", async () => {
+    const newUser = {
+      name: "User with error",
+      email: "error@user.com",
+      password: 123456,
+    };
+
+    const response = await request(server)
+      .post("/user")
+      .send(newUser);
+
+    expect(response.status).toBe(409);
+  });
+
+  afterAll(async () => {
+    server.close();
+  });
+});
