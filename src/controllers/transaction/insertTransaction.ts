717fa77e7b7887c5e04e2cc2ec8d765f3fa53568
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-22 20:52:57 -0300

    feat(insertTransaction.ts): Cria controlador para rota de adicionar uma transação

---
+++ src/controllers/transaction/insertTransaction.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { handleError } from "../../utils";
+import { TransactionParams } from "../../models";
+import { insertTransactionAndReturn } from "../../services/transaction";
+
+const insertTransaction = async (req: Request, res: Response) => {
+  try {
+    const { type, description, value, date, categorie_id } = req.body as TransactionParams;
+
+    const transaction = {
+      type,
+      description,
+      value,
+      date,
+      categorie_id
+    };
+
+    const newTransaction = await insertTransactionAndReturn(transaction, req);
+
+    return res.status(201).json(newTransaction);
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+};
+
+export default insertTransaction;
