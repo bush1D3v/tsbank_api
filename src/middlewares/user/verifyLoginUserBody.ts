b12cecaab69128850e809c5afabdfc865f5730b1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-23 19:53:14 -0300

    update(verifyLoginUserBody.ts): Atualizando chamada da funcao de erro

--- src/middlewares/user/verifyLoginUserBody.ts
+++ src/middlewares/user/verifyLoginUserBody.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { handleError } from "../../utils";
+import { handleError } from "../../error";
 import { loginUserSchema } from "../../schemas";
 
 const verifyLoginUserBody = async (req: Request, res: Response, next: NextFunction) => {
