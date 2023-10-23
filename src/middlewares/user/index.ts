91ba7de51cfb6b10b12f4cb032e576ef2c260b34
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-23 20:35:23 -0300

    update(index.ts): Atualiza exportacao de middlewares relacionados a usuario

--- src/middlewares/user/index.ts
+++ src/middlewares/user/index.ts
@@ -1,5 +1,4 @@
 import verifyUserBody from "./verifyUserBody";
 import verifyLoginUserBody from "./verifyLoginUserBody";
-import verifyEmailExists from "./verifyEmailExists";
 
-export { verifyUserBody, verifyLoginUserBody, verifyEmailExists };
+export { verifyUserBody, verifyLoginUserBody };
