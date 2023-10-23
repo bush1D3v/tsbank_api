1fedc62dde9d664be025cb6f28caa6f2e02bcdcd
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-23 20:22:27 -0300

    feat(getTransactionsAndReturn.ts): Cria servico para controlador da rota de exibir transacoes do usuario

---
+++ src/services/transaction/getTransactionsAndReturn.ts
@@ -0,0 +1,17 @@
+import { Request } from "express";
+import { getToken } from "../../utils";
+import { getTransactions } from "../../repositories";
+
+const getTransactionsAndReturn = async (req: Request) => {
+  const id: number = getToken(req);
+
+  const transactions: string[] = await getTransactions(id);
+
+  if (transactions.length < 1) {
+    throw new Error("Your account does not have any registered transactions.");
+  }
+
+  return transactions;
+};
+
+export default getTransactionsAndReturn;
