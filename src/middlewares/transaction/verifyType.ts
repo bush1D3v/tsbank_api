c21957ac640e016e57dbf400d28094d6e00ef6aa
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-23 20:14:26 -0300

    update(verifyType.ts): Atualiza chamada de funcao de erro

--- src/middlewares/transaction/verifyType.ts
+++ src/middlewares/transaction/verifyType.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { handleError } from "../../utils";
+import { handleError } from "../../error";
 
 const verifyType = (req: Request, res: Response, next: NextFunction) => {
   try {
