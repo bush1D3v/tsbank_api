52acc21f6b4b04438dc38249b9a421aad3f7f521
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-24 18:41:20 -0300

    update(index.ts): Atualiza exportacao de modelos relacionados a usuario

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -1,5 +1,11 @@
 import { UserParams } from "./UserParams";
 import { LoginUserParams } from "./LoginUserParams";
 import { DatabaseUserParams } from "./DatabaseUserParams";
+import { ReturnedDatabaseUserParams } from "./ReturnedDatabaseUserParams";
 
-export { UserParams, LoginUserParams, DatabaseUserParams };
+export {
+  UserParams,
+  LoginUserParams,
+  DatabaseUserParams,
+  ReturnedDatabaseUserParams
+};
