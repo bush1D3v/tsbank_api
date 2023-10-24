8ed20ebd1a489eb385e1f890f5b0882a4263068c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-24 16:56:46 -0300

    refactor(insertTransactionAndReturn.ts): Refatorando passagem de parametro para a funcao verifyCategorieId

--- src/services/transaction/insertTransactionAndReturn.ts
+++ src/services/transaction/insertTransactionAndReturn.ts
@@ -5,7 +5,7 @@ import { TransactionParams } from "../../models";
 import { verifyCategorieId } from "../../providers";
 
 const insertTransactionAndReturn = async (params: TransactionParams, req: Request) => {
-  await verifyCategorieId(req);
+  await verifyCategorieId(params.categorie_id);
 
   const userId = getToken(req);
 
