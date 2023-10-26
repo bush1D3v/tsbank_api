d61a65f4f6c540bd092da153d345dbf42b66c768
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-26 01:30:16 -0300

    update(getUserPerId.ts): Adiciona verificacao de existencia do usuario

--- src/repositories/user/getUserPerId.ts
+++ src/repositories/user/getUserPerId.ts
@@ -1,8 +1,11 @@
 import db from "../../data/connection";
 import { DatabaseUserParams } from "../../models";
+import { undefinedUser } from "../../providers";
 
 const getUserPerId = async (id: number) => {
   const user: DatabaseUserParams = await db("users").where({ id }).first();
+
+  undefinedUser(user);
 
   const { password: _, ...userResponse } = user;
 
