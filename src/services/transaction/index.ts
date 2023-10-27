4c4c529e93fbb41c70ee8d7b82e17c3de088ecfe
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-26 23:40:03 -0300

    update(index.ts): Atualiza exportacao de servicos relacionados a transacao

--- src/services/transaction/index.ts
+++ src/services/transaction/index.ts
@@ -1,9 +1,11 @@
 import getTransactionsAndReturn from "./getTransactionsAndReturn";
 import insertTransactionAndReturn from "./insertTransactionAndReturn";
 import getTransactionAndReturn from "./getTransactionAndReturn";
+import updateTransactionAndReturn from "./updateTransactionAndReturn";
 
 export {
   insertTransactionAndReturn,
   getTransactionsAndReturn,
-  getTransactionAndReturn
+  getTransactionAndReturn,
+  updateTransactionAndReturn
 };
