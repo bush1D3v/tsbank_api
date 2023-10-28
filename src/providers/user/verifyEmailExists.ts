2964adfa5e7d377f639c76414c7cccbb80849a5d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-28 17:29:05 -0300

    refactor(verifyEmailExists.ts): Atualizando exportacao da funcao

--- src/providers/user/verifyEmailExists.ts
+++ src/providers/user/verifyEmailExists.ts
@@ -1,12 +1,10 @@
 import { HttpStatusError } from "../../error";
 import { validateEmail } from "../../repositories";
 
-const verifyEmailExists = async (email: string) => {
+export default async function verifyEmailExists(email: string) {
   const emailExists = await validateEmail(email);
 
   if (emailExists) {
     throw new HttpStatusError("Email already exists", 409);
   }
 };
-
-export default verifyEmailExists;
