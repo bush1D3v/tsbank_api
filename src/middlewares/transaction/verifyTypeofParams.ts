af0f537b4d05dad408dfca3010309e1dcee541e2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-28 17:45:38 -0300

    feat(verifyTypeofParams.ts): Cria middleware que verifica tipo de parametro da url

---
+++ src/middlewares/transaction/verifyTypeofParams.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { handleError } from "../../error";
+
+export default function verifyTypeofParams(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(Number(id))) {
+      throw new Error("Invalid value of 'id' parameter");
+    }
+
+    next();
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+}
