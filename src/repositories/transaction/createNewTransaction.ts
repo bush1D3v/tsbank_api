af72bad5b9d21003f552063150d59df75d5c1315
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-28 17:29:22 -0300

    refactor(createNewTransaction.ts): Atualizando exportacao da funcao

--- src/repositories/transaction/createNewTransaction.ts
+++ src/repositories/transaction/createNewTransaction.ts
@@ -1,16 +1,14 @@
 import db from "../../data/connection";
 import { DatabaseTransactionParams, TransactionParams } from "../../models";
 
-const createNewTransaction = async (params: TransactionParams, id: number) => {
+export default async function createNewTransaction(params: TransactionParams, id: number) {
   const newTransaction: DatabaseTransactionParams[] = await db("transactions").insert({
     type: params.type,
     description: params.description,
     value: params.value,
-    categorie_id: params.categorie_id,
+    category_id: params.category_id,
     user_id: id
   }).returning("*");
 
   return newTransaction[ 0 ];
 };
-
-export default createNewTransaction;
