15a7d405de7836fc3fc5b8f17ade56cd3a6e6b15
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-28 17:42:32 -0300

    update(index.ts): Atualiza exportacao de controladores relacionados a transacao

--- src/controllers/transaction/index.ts
+++ src/controllers/transaction/index.ts
@@ -2,10 +2,12 @@ import insertTransaction from "./insertTransaction";
 import listTransactions from "./listTransactions";
 import detailTransaction from "./detailTransaction";
 import updateTransaction from "./updateTransaction";
+import deleteTransaction from "./deleteTransaction";
 
 export {
   insertTransaction,
   listTransactions,
   detailTransaction,
-  updateTransaction
+  updateTransaction,
+  deleteTransaction
 };
