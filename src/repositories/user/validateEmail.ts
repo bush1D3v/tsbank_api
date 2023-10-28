ed7c198e7c9c17d81a6dd8547714f5461eca8285
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-28 17:31:21 -0300

    refactor(validateEmail.ts): Atualizando exportacao da funcao

--- src/repositories/user/validateEmail.ts
+++ src/repositories/user/validateEmail.ts
@@ -1,10 +1,8 @@
 import db from "../../data/connection";
 import { DatabaseUserParams } from "../../models";
 
-const validateEmail = async (email: string) => {
+export default async function validateEmail(email: string) {
   const user: DatabaseUserParams = await db("users").where({ email }).first();
 
   return user;
 };
-
-export default validateEmail;
