19bf86c31b456e596fd2abcbace606e9e7991d3c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-28 17:22:20 -0300

    refactor(HttpStatusError.ts): Atualizando exportacao da funcao

--- src/error/HttpStatusError.ts
+++ src/error/HttpStatusError.ts
@@ -1,10 +1,8 @@
-class HttpStatusError extends Error {
+export default class HttpStatusError extends Error {
   statusCode: number;
 
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
   }
-}
-
-export default HttpStatusError;
+};
