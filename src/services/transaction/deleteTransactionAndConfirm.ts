16fad5fa83e35454fcd575d73bb1aecc9deb492d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 16:00:29 -0300

    update(deleteTransactionAndConfirm.ts): Adiciona verificacoes e exclusao de dados

--- src/services/transaction/deleteTransactionAndConfirm.ts
+++ src/services/transaction/deleteTransactionAndConfirm.ts
@@ -1,9 +1,14 @@
 import { Request } from "express";
 import { dropTransaction } from "../../repositories";
-import getTransactionAndReturn from "./getTransactionAndReturn";
+import { validateTransaction, verifyTransactionId } from "../../providers";
+import { getToken } from "../../utils";
 
 export default async function deleteTransactionAndConfirm(req: Request, transaction_id: number) {
-  await getTransactionAndReturn(req, transaction_id);
+  await verifyTransactionId(transaction_id);
+
+  const userId = getToken(req);
+
+  await validateTransaction(transaction_id, userId);
 
   await dropTransaction(transaction_id);
 };
