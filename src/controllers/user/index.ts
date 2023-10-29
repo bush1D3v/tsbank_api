332cdcc58aa8ae7762a7303548ce2af3b26cfb84
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 14:48:53 -0300

    update(index.ts): Atualiza exportacao de controladores relacionados a usuario

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -2,5 +2,16 @@ import insertUser from "./insertUser";
 import loginUser from "./loginUser";
 import detailUser from "./detailUser";
 import updateUser from "./updateUser";
+import updateUserEmail from "./updateUserEmail";
+import updateUserPassword from "./updateUserPassword";
+import deleteUser from "./deleteUser";
 
-export { insertUser, loginUser, detailUser, updateUser };
+export {
+  insertUser,
+  loginUser,
+  detailUser,
+  updateUser,
+  updateUserEmail,
+  updateUserPassword,
+  deleteUser
+};
