afe9c1805707e414640804b01301973f4c693ab5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 17:48:43 -0300

    refactor(verifyTransactionBody.ts): Refatora chamada de funcao validadora

--- src/middlewares/transaction/verifyTransactionBody.ts
+++ src/middlewares/transaction/verifyTransactionBody.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { handleError } from "../../error";
-import { transactionSchema } from "../../schemas";
+import { TransactionSchema } from "../../schemas";
 
 export default async function verifyTransactionBody(req: Request, res: Response, next: NextFunction) {
   try {
-    await transactionSchema.validate(req.body);
+    await TransactionSchema.validate(req.body);
 
     next();
   } catch (error: any) {
