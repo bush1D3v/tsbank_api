18155be6ae1536c6e46eb655c0987346b27cf564
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 14:51:13 -0300

    updateUserPassword.ts): Cria controlador para rota de atualizar senha do usuario

---
+++ src/controllers/user/updateUserPassword.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { handleError } from "../../error";
+import { UpdateUserPasswordParams } from "../../models";
+import { updatePasswordAndConfirm } from "../../services";
+
+export default async function updateUserPassword(req: Request, res: Response) {
+  try {
+    const { password, new_password } = req.body as UpdateUserPasswordParams;
+
+    const params = {
+      password,
+      new_password
+    };
+
+    await updatePasswordAndConfirm(req, params);
+
+    return res.status(201).json({
+      "message": "Your password has been changed successfully"
+    });
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+};
