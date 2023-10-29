761e1346b3bf5c3be9d34dd4eb392e50f31d9c1b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 15:09:07 -0300

    update(index.ts): Atualiza exportacao de repositorios relacionados a usuario

--- src/repositories/user/index.ts
+++ src/repositories/user/index.ts
@@ -2,12 +2,18 @@ import createNewUser from "./createNewUser";
 import validatePassword from "./validatePassword";
 import validateEmail from "./validateEmail";
 import getUserPerId from "./getUserPerId";
-import updateUser from "./updateUser";
+import refreshUser from "./refreshUser";
+import refreshUserEmail from "./refreshUserEmail";
+import refreshUserPassword from "./refreshUserPassword";
+import eraseUser from "./eraseUser";
 
 export {
   createNewUser,
   validatePassword,
   validateEmail,
   getUserPerId,
-  updateUser
+  refreshUser,
+  refreshUserEmail,
+  refreshUserPassword,
+  eraseUser
 };
