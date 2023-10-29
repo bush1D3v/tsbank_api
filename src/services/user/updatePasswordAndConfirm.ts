d2185b36be35c485c786b26dde0f0fde7129e508
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 15:27:41 -0300

    feat(updatePasswordAndConfirm.ts): Cria servico que atualiza senha apos validacao dos dados da conta

---
+++ src/services/user/updatePasswordAndConfirm.ts
@@ -0,0 +1,16 @@
+import { Request } from "express";
+import { encryptPassword, getToken } from "../../utils";
+import { getUserPerId, refreshUserPassword, validatePassword } from "../../repositories";
+import { UpdateUserPasswordParams } from "../../models";
+
+export default async function updatePasswordAndConfirm(req: Request, params: UpdateUserPasswordParams) {
+  const id = getToken(req);
+
+  const user = await getUserPerId(id);
+
+  await validatePassword(params.password, user.password);
+
+  const newPassword = await encryptPassword(params.password);
+
+  await refreshUserPassword(user.password, newPassword);
+};
