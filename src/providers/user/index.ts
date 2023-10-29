b199324e564c0ce00d389a66790eade7fc23cd27
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 15:02:20 -0300

    update(index.ts): Atualiza exportacao de provedores relacionados a usuario

--- src/providers/user/index.ts
+++ src/providers/user/index.ts
@@ -1,4 +1,4 @@
-import verifyEmailExists from "./verifyEmailExists";
 import undefinedUser from "./undefinedUser";
+import verifyEmailExists from "./verifyEmailExists";
 
 export { verifyEmailExists, undefinedUser };
