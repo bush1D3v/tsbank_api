c540cb8a69b6249d62dda2c07575d7ab4b84b7fa
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 15:13:40 -0300

    update(index.ts): Atualiza exportacao de servicos relacionados a usuario

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -2,10 +2,16 @@ import insertUserAndReturn from "./insertUserAndReturn";
 import loginUserAndReturn from "./loginUserAndReturn";
 import getUserDetailsAndReturn from "./getUserDetailsAndReturn";
 import updateUserAndReturn from "./updateUserAndReturn";
+import updateEmailAndConfirm from "./updateEmailAndConfirm";
+import updatePasswordAndConfirm from "./updatePasswordAndConfirm";
+import deleteUserAndConfirm from "./deleteUserAndConfirm";
 
 export {
   insertUserAndReturn,
   loginUserAndReturn,
   getUserDetailsAndReturn,
-  updateUserAndReturn
+  updateUserAndReturn,
+  updateEmailAndConfirm,
+  updatePasswordAndConfirm,
+  deleteUserAndConfirm
 };
