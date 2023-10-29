d542cfcd1e7368a5f93e953f9d66f4636e5eace2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 17:42:46 -0300

    refactor(TransactionSchema.ts): Atualizando nome da funcao

--- src/schemas/transaction/TransactionSchema.ts
+++ src/schemas/transaction/TransactionSchema.ts
@@ -1,10 +1,10 @@
 import * as yup from "yup";
 
-const transactionSchema = yup.object({
+const TransactionSchema = yup.object({
   type: yup.string().required(),
   description: yup.string().required(),
   value: yup.number().required(),
   category_id: yup.number().required()
 });
 
-export default transactionSchema;
+export default TransactionSchema;
