13ef3c54a8133b44e6b830a2b88b979eb57cd120
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-29 16:15:56 -0300

    update(deleteTransactionAndConfirm.ts): Cria validacao de senha do usuario

--- src/services/transaction/deleteTransactionAndConfirm.ts
+++ src/services/transaction/deleteTransactionAndConfirm.ts
@@ -1,12 +1,16 @@
 import { Request } from "express";
-import { dropTransaction } from "../../repositories";
+import { dropTransaction, getUserPerId, validatePassword } from "../../repositories";
 import { validateTransaction, verifyTransactionId } from "../../providers";
 import { getToken } from "../../utils";
 
-export default async function deleteTransactionAndConfirm(req: Request, transaction_id: number) {
+export default async function deleteTransactionAndConfirm(req: Request, transaction_id: number, password: string) {
   await verifyTransactionId(transaction_id);
 
   const userId = getToken(req);
+
+  const user = await getUserPerId(userId);
+
+  await validatePassword(password, user.password);
 
   await validateTransaction(transaction_id, userId);
 
