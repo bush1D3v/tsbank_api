cb106607f9861a2e13b4f1f16feb3f1c555ecebe
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-30 22:33:02 -0300

    update(index.ts): Atualiza exportacao de middlewares relacionados a transacao

--- src/middlewares/transaction/index.ts
+++ src/middlewares/transaction/index.ts
@@ -1,5 +1,11 @@
 import verifyTransactionBody from "./verifyTransactionBody";
+import verifyTransactionValue from "./verifyTransactionValue";
 import verifyType from "./verifyType";
 import verifyTypeofParams from "./verifyTypeofParams";
 
-export { verifyTransactionBody, verifyType, verifyTypeofParams };
+export {
+  verifyTransactionBody,
+  verifyType,
+  verifyTypeofParams,
+  verifyTransactionValue
+};
