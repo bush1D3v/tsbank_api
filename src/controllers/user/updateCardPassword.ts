298ad90343239d1cb5ed2bafd3942fdd7bc22193
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 19:43:49 -0300

    feat(updateCardPassword.ts): Cria controlador para atualizar a senha do cartao

---
+++ src/controllers/user/updateCardPassword.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { handleError } from "../../error";
+import { UpdateCardPasswordParams } from "../../models";
+import { updateCardPasswordAndConfirm } from "../../services";
+
+export default async function updateCardPassword(req: Request, res: Response) {
+  try {
+    const { card_type, password, new_password } = req.body as UpdateCardPasswordParams;
+
+    const params = {
+      card_type,
+      password,
+      new_password
+    };
+
+    await updateCardPasswordAndConfirm(req, params);
+
+    return res.status(201).json({
+      message: `Password of your ${card_type} card has been updated successfully`
+    });
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+};
