e07a14ad23ee21326d39b70bfee1637e626627c6
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:21:15 -0300

    feat(removeValue.ts): Cria repositorio que remove saldo do usuario ou cartao de credito

---
+++ src/repositories/transaction/removeValue.ts
@@ -0,0 +1,8 @@
+import db from "../../data/connection";
+
+export default async function removeValue(accountItem: string, value: number, user_id: number) {
+  if (accountItem.toLowerCase() === "credit") {
+    await db("credit_cards").decrement("balance", value).where({ user_id });
+  }
+  await db("users").decrement(accountItem, value).where({ id: user_id });
+};
