a256c56f377a76b25e059b2de55aa6c80cb1c9fa
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:53:49 -0300

    feat(makeCardPayAndConfirm.ts): Cria servico que faz pagamento do cartao e retorna transacao criada

---
+++ src/services/transaction/makeCardPayAndConfirm.ts
@@ -0,0 +1,32 @@
+import { Request } from "express";
+import { OutputTransactionParams } from "../../models";
+import { getToken } from "../../utils";
+import {
+  cardPay,
+  createNewTransaction,
+  getUserPerId,
+  validateOutput,
+  validatePassword
+} from "../../repositories";
+
+export default async function makeCardPayAndConfirm(req: Request, params: OutputTransactionParams) {
+  const userId = getToken(req);
+
+  const user = await getUserPerId(userId);
+
+  await validatePassword(params.password, user.password);
+
+  validateOutput(user.balance, params.value);
+
+  await cardPay(params.value, userId);
+
+  const transaction = {
+    type: "output",
+    description: "card pay",
+    value: params.value
+  };
+
+  const transactionResponse = await createNewTransaction(transaction, userId);
+
+  return transactionResponse;
+};
