efe62612ebc84ac48b1a1b38d1d6ee221f79ac1b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:15:14 -0300

    feat(verifyPhoneExists.ts): Cria provedor que verifica existencia de telefone

---
+++ src/providers/user/verifyPhoneExists.ts
@@ -0,0 +1,10 @@
+import { HttpStatusError } from "../../error";
+import { validatePhone } from "../../repositories";
+
+export default async function verifyPhoneExists(phone: string) {
+  const phoneExists = await validatePhone(phone);
+
+  if (phoneExists) {
+    throw new HttpStatusError("This phone already used per other user", 409);
+  }
+};
