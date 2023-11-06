41285a0eab58a54bc1dde5bf2739617c0bdab007
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 22:48:59 -0300

    feat(updateUserPhone.ts): Cria controlador para atualizar telefone do usuario

---
+++ src/controllers/user/updateUserPhone.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { handleError } from "../../error";
+import { updatePhoneAndConfirm } from "../../services";
+import { UpdateUserPhoneParams } from "../../models";
+
+export default async function updateUserPhone(req: Request, res: Response) {
+  try {
+    const { password, new_phone } = req.body as UpdateUserPhoneParams;
+
+    const params = {
+      password,
+      new_phone
+    };
+
+    const { phone } = await updatePhoneAndConfirm(req, params);
+
+    return res.status(201).json({
+      "message": `Your phone has been changed successfully, now is ${phone}`
+    });
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+};
