e58fbe01dbe44c5beea2a284c1b754dd375e0641
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:48:57 -0300

    update(loginUserAndReturn.ts): Atualizando passagem de parametros para o servico

--- src/services/user/loginUserAndReturn.ts
+++ src/services/user/loginUserAndReturn.ts
@@ -1,15 +1,16 @@
 import { HttpStatusError } from "../../error";
+import { LoginUserParams } from "../../models";
 import { validateEmail, validatePassword } from "../../repositories";
 import { createToken } from "../../utils";
 
-export default async function loginUserAndReturn(email: string, password: string) {
-  const user = await validateEmail(email);
+export default async function loginUserAndReturn(params: LoginUserParams) {
+  const user = await validateEmail(params.email);
 
   if (typeof user === "undefined") {
     throw new HttpStatusError("Invalid email", 401);
   }
 
-  await validatePassword(password, user.password);
+  await validatePassword(params.password, user.password);
 
   const response = createToken(user);
 
