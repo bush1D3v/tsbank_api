fdc5e0c71f2f30ec8a6ad0b66660e74eaccabba0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:06:23 -0300

    update(index.ts): Atualiza exportacao de modelos relacionados a transacao

--- src/models/transaction/index.ts
+++ src/models/transaction/index.ts
@@ -1,4 +1,21 @@
+import { CardPayParams } from "./CardPayParams";
+import { CardTransactionParams } from "./CardTransactionParams";
 import { DatabaseTransactionParams } from "./DatabaseTransactionParams";
+import { DeleteTransactionParams } from "./DeleteTransactionParams";
+import { DepositParams } from "./DepositParams";
+import { NewPixParams } from "./NewPixParams";
+import { OutputTransactionParams } from "./OutputTransactionParams";
+import { PixParams } from "./PixParams";
 import { TransactionParams } from "./TransactionParams";
 
-export { TransactionParams, DatabaseTransactionParams };
+export {
+  TransactionParams,
+  DatabaseTransactionParams,
+  DeleteTransactionParams,
+  OutputTransactionParams,
+  CardTransactionParams,
+  PixParams,
+  DepositParams,
+  CardPayParams,
+  NewPixParams
+};
