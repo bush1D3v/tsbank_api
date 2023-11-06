4d696c7d1791c732f9997f0862a0793debad89da
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:42:02 -0300

    update(index.ts): Atualiza exportacao de esquemas relacionados a usuario

--- src/schemas/user/index.ts
+++ src/schemas/user/index.ts
@@ -5,6 +5,10 @@ import UpdateEmailSchema from "./UpdateEmailSchema";
 import UpdatePasswordSchema from "./UpdatePasswordSchema";
 import UpdateUserSchema from "./UpdateUserSchema";
 import PasswordSchema from "./PasswordSchema";
+import CardSchema from "./CardSchema";
+import GeneralCardSchema from "./GeneralCardSchema";
+import UpdateCardPasswordSchema from "./UpdateCardPasswordSchema";
+import UpdatePhoneSchema from "./UpdatePhoneSchema";
 
 export {
   UserSchema,
@@ -13,5 +17,9 @@ export {
   UpdateEmailSchema,
   UpdatePasswordSchema,
   UpdateUserSchema,
-  PasswordSchema
+  PasswordSchema,
+  GeneralCardSchema,
+  CardSchema,
+  UpdateCardPasswordSchema,
+  UpdatePhoneSchema
 };
