5e60bda86e31273c722a388ff616991f80299079
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:16:28 -0300

    feat(createNewDeposit.ts): Cria repositorio que efetua um deposito

--- src/repositories/transaction/createNewDeposit.ts
+++ src/repositories/transaction/createNewDeposit.ts
@@ -1,5 +1,5 @@
 import db from "../../data/connection";
 
-export default async function createNewDeposit(id: number, value: number) {
-  await db("users").increment({ "balance": value }).where({ id });
-}
+export default async function createNewDeposit(email: string, value: number) {
+  await db("users").increment({ "balance": value }).where({ email });
+};
