7f86962e9100a452ca9d01629c918cefaf6c2136
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:22:34 -0300

    update(createNewUser.ts): Atualiza valores a adicionar no usuario

--- src/repositories/user/createNewUser.ts
+++ src/repositories/user/createNewUser.ts
@@ -4,9 +4,11 @@ import { ReturnedDatabaseUserParams, UserParams } from "../../models";
 export default async function createNewUser(params: UserParams) {
   const user: ReturnedDatabaseUserParams[] = await db("users")
     .insert({
-      name: params[ "name" ],
-      email: params[ "email" ],
-      password: params[ "password" ]
+      name: params.name,
+      email: params.email,
+      password: params.password,
+      cpf: params.cpf,
+      phone: params.phone
     })
     .returning([ "id", "name", "email" ]);
 
