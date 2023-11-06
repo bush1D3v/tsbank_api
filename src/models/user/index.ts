efedaf7308668d4b7a9795b65721435437f76e02
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:10:38 -0300

    update(index.ts): Atualiza exportacao de modelos relacionados a usuario

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -2,16 +2,32 @@ import { UpdateUserParams } from "./UpdateUserParams";
 import { LoginUserParams } from "./LoginUserParams";
 import { DatabaseUserParams } from "./DatabaseUserParams";
 import { ReturnedDatabaseUserParams } from "./ReturnedDatabaseUserParams";
-import { UpdateUserMailParams } from "./UpdateUserMailParams";
+import { UpdateUserEmailParams } from "./UpdateUserEmailParams";
 import { UpdateUserPasswordParams } from "./UpdateUserPasswordParams";
 import { UserParams } from "./UserParams";
+import { InsertCardParams } from "./InsertCardParams";
+import { CardParams } from "./CardParams";
+import { ValidateCardParams } from "./ValidateCardParams";
+import { DatabaseCardParams } from "./DatabaseCardParams";
+import { UpdateCardPasswordParams } from "./UpdateCardPasswordParams";
+import { RefreshCardParams } from "./RefreshCardParams";
+import { UpdateUserPhoneParams } from "./UpdateUserPhoneParams";
+import { DeleteUserParams } from "./DeleteUserParams";
 
 export {
   UpdateUserParams,
   LoginUserParams,
   DatabaseUserParams,
   ReturnedDatabaseUserParams,
-  UpdateUserMailParams,
+  UpdateUserEmailParams,
   UpdateUserPasswordParams,
-  UserParams
+  UserParams,
+  InsertCardParams,
+  CardParams,
+  ValidateCardParams,
+  DatabaseCardParams,
+  UpdateCardPasswordParams,
+  RefreshCardParams,
+  UpdateUserPhoneParams,
+  DeleteUserParams
 };
