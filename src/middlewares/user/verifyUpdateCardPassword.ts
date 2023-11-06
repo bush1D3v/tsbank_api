c794653a438dcb5f1b34c143a45f89f3fc201ebb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 22:53:37 -0300

    feat(verifyUpdateCardPassword.ts): Cria middleware que verifica corpo da requisicao de rota que atualiza senha do cartao

---
+++ src/middlewares/user/verifyUpdateCardPassword.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { handleError } from "../../error";
+import { UpdateCardPasswordSchema } from "../../schemas";
+import { validateCardType } from "../../repositories";
+
+export default async function verifyUpdateCardPassword(req: Request, res: Response, next: NextFunction) {
+  try {
+    await UpdateCardPasswordSchema.validate(req.body);
+
+    const { card_type } = req.body;
+
+    validateCardType(card_type);
+
+    next();
+  } catch (error: any) {
+    handleError(res, error, 400);
+  }
+};
