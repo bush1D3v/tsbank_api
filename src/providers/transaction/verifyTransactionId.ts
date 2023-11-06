472d7ec93450ea7e5ad271803611ccdaaa39fd60
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:12:01 -0300

    feat(VerifyTransactionId.ts): Cria provedor que verifica existencia da transacao

--- src/providers/transaction/verifyTransactionId.ts
+++ src/providers/transaction/verifyTransactionId.ts
@@ -1,8 +1,8 @@
 import { HttpStatusError } from "../../error";
 import { getTransaction } from "../../repositories";
 
-export default async function verifyTransactionId(id: Number) {
-  const transaction = await getTransaction(id);
+export default async function verifyTransactiontransaction_id(transaction_id: Number) {
+  const transaction = await getTransaction(transaction_id);
 
   if (!transaction) {
     throw new HttpStatusError("Transaction not found", 404);
