f57f526d24f5af5a6fc0938a570ce7b0971152d1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:50:42 -0300

    update(insertAndReturn.ts): Atualiza parametros da funcao

--- src/services/user/insertUserAndReturn.ts
+++ src/services/user/insertUserAndReturn.ts
@@ -1,17 +1,23 @@
 import { UserParams } from "../../models";
-import { verifyEmailExists } from "../../providers";
+import { verifyCpfExists, verifyEmailExists, verifyPhoneExists } from "../../providers";
 import { createNewUser } from "../../repositories";
 import { encryptPassword } from "../../utils";
 
 export default async function insertUserAndReturn(params: UserParams) {
   await verifyEmailExists(params.email);
 
+  await verifyCpfExists(params.cpf);
+
+  await verifyPhoneExists(params.phone);
+
   const cryptPassword = await encryptPassword(params.password);
 
   const newUser = {
     name: params.name,
     email: params.email,
-    password: cryptPassword
+    password: cryptPassword,
+    cpf: params.cpf,
+    phone: params.phone
   };
 
   const user = await createNewUser(newUser);
