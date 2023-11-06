06247f7b91d87d105e43cf17ff15715d2af23dcf
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:24:15 -0300

    update(refreshUserEmail.ts): Adiciona retorno de email colocado no usuario

--- src/repositories/user/refreshUserEmail.ts
+++ src/repositories/user/refreshUserEmail.ts
@@ -1,8 +1,14 @@
 import db from "../../data/connection";
 
+type EmailDate = {
+  email: string
+}
+
 export default async function refreshUserEmail(password: string, new_email: string) {
-  await db("users").update({ "email": new_email })
+  const returnedEmail: EmailDate[] = await db("users").update({ "email": new_email })
     .where({
       password
-    });
+    }).returning([ "email" ]);
+
+  return returnedEmail[ 0 ];
 };
