a305cbda2feb7b335c0f44c07bc78638bae1fde0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-05 23:13:53 -0300

    feat(validateCard.ts): Cria provedor que valida adicao de cartao

---
+++ src/providers/user/validateCard.ts
@@ -0,0 +1,14 @@
+import db from "../../data/connection";
+import { HttpStatusError } from "../../error";
+import { DatabaseCardParams, ValidateCardParams } from "../../models";
+
+export default async function validateCard(params: ValidateCardParams) {
+  const cardExists: DatabaseCardParams[] = await db(params.card_type.toLowerCase() + "_cards")
+    .where({
+      user_id: params.user_id
+    });
+
+  if (cardExists.length > 0) {
+    throw new HttpStatusError(`This user already have a ${params.card_type} card`, 409);
+  }
+};
