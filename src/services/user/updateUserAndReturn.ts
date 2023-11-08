6e3ba00f8ae9363fe573c897d4409ecab2eaf7e8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-08 18:36:34 -0300

    refactor(updateUserAndreturn.ts): Refatorando passagem de parametros para a atualizacao do usuario

--- src/services/user/updateUserAndReturn.ts
+++ src/services/user/updateUserAndReturn.ts
@@ -3,25 +3,28 @@ import { getUserPerId, refreshUser, validatePassword } from "../../repositories"
 import { encryptPassword } from "../../utils";
 import { Request } from "express";
 import { getToken } from "../../utils";
-import { verifyEmailExists } from "../../providers";
+import { verifyEmailExists, verifyPhoneExists } from "../../providers";
 
 export default async function updateUserAndReturn(req: Request, params: UpdateUserParams) {
   await verifyEmailExists(params.new_email);
 
-  const id = getToken(req);
+  await verifyPhoneExists(params.new_phone);
 
-  const user = await getUserPerId(id);
+  const userId = getToken(req);
+
+  const user = await getUserPerId(userId);
 
   await validatePassword(params.password, user.password);
 
   const cryptPassword = await encryptPassword(params.new_password);
 
   const refreshed = {
-    email: params.new_email,
-    password: cryptPassword
+    new_email: params.new_email,
+    new_password: cryptPassword,
+    new_phone: params.new_phone
   };
 
-  const newUser = await refreshUser(refreshed, id);
+  const newUser = await refreshUser(refreshed, userId);
 
   return newUser;
 };
