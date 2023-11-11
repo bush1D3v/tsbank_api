ccbaab7f167aa494af03d47b4f3549e76def72fb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-11 11:21:12 -0300

    refactor(getUserPerId.ts): Refatorando o erro de usuario nao encontrado

--- src/repositories/user/getUserPerId.ts
+++ src/repositories/user/getUserPerId.ts
@@ -6,7 +6,7 @@ export default async function getUserPerId(id: number) {
   const user: DatabaseUserParams = await db("users").where({ id }).first();
 
   if (typeof user === "undefined") {
-    throw new HttpStatusError("User not found", 404);
+    throw new HttpStatusError("user not found", 404);
   }
 
   return user;
