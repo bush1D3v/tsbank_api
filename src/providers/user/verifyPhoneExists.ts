50f7f365621ac1a2db3b3e8b18e1a688cf2dc3c7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-11 11:22:47 -0300

    refactor(verifyPhoneExists.ts): Refatorando chamada da funcao de pegar usuario pelo telefone

--- src/providers/user/verifyPhoneExists.ts
+++ src/providers/user/verifyPhoneExists.ts
@@ -1,8 +1,8 @@
 import { HttpStatusError } from "../../error";
-import { validatePhone } from "../../repositories";
+import { getUserPerPhone } from "../../repositories";
 
 export default async function verifyPhoneExists(phone: string) {
-  const phoneExists = await validatePhone(phone);
+  const phoneExists = await getUserPerPhone(phone);
 
   if (phoneExists) {
     throw new HttpStatusError("this phone already used per other user", 409);
