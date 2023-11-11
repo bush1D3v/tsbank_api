c5d2ce923abc2c660e56d0a9c7cf36183390c8a6
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-11 11:19:29 -0300

    refactor(getUserPerPhone.ts): Melhorando o nome do repositorio

--- src/repositories/user/getUserPerPhone.ts
+++ src/repositories/user/getUserPerPhone.ts
@@ -1,7 +1,7 @@
 import db from "../../data/connection";
 import { DatabaseUserParams } from "../../models";
 
-export default async function validatePhone(phone: string) {
+export default async function getUserPerPhone(phone: string) {
   const user: DatabaseUserParams = await db("users").where({ phone }).first();
 
   return user;
