5fb8b02b9c9639e6f164f26a7cd0dc886ffc5cfa
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:52:21 -0300

    fix(testsSetup.ts): Corrige ordem de exclusão dos dados do banco de testes

--- tests/testsSetup.ts
+++ tests/testsSetup.ts
@@ -2,10 +2,10 @@ import db from "../src/data/connection";
 import server from "../src/server";
 
 afterAll(async () => {
-  await db("users").delete("*");
-  await db("transactions").delete("*");
-  await db("credit_cards").delete("*");
   await db("debit_cards").delete("*");
+  await db("credit_cards").delete("*");
+  await db("transactions").delete("*");
+  await db("users").delete("*");
   await db.destroy();
   server.close();
 });
