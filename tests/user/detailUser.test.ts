b0e208f3418ed2b1374e451e6df50408b1980cd9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:44:40 -0300

    test(detaliUser.test.ts): Cria todos os testes para a rota de detalhar usuario

--- tests/user/detailUser.test.ts
+++ tests/user/detailUser.test.ts
@@ -1,32 +1,14 @@
 import request from "supertest";
 import server from "../../src/server";
-import { UserParams } from "../../src/models";
-
-const user: UserParams = {
-  name: "Victor Navarro",
-  cpf: "12345678931",
-  phone: "21123456789",
-  email: "victorjln@gmail.com",
-  password: "vtjln123"
-};
-
-const loginUser = {
-  email: "victorjln@gmail.com",
-  password: "vtjln123"
-};
+import { loginUser, user } from "../models";
+import {
+  deleteUser,
+  insertUserAndLogin,
+  unauthUser
+} from "../functions";
 
 let bearerToken: string | undefined;
 let response: request.Response;
-
-const deleteUser = async (token: string | undefined) => {
-  const password = user.password;
-  response = await request(server)
-    .delete("/user")
-    .set("Authorization", `Bearer ${token}`)
-    .send({ "password": password });
-
-  return response;
-};
 
 const detailUser = async (token: string | undefined) => {
   response = await request(server)
@@ -37,25 +19,9 @@ const detailUser = async (token: string | undefined) => {
   return response;
 };
 
-const unauthUser = async () => {
-  response = await request(server)
-    .get("/user")
-    .send();
-
-  return response;
-};
-
 describe("Detail User Controller Tests", () => {
   beforeAll(async () => {
-    await request(server)
-      .post("/user")
-      .send(user);
-
-    const tokenReq = await request(server)
-      .post("/login")
-      .send(loginUser);
-
-    bearerToken = tokenReq.body.token;
+    bearerToken = await insertUserAndLogin(user, loginUser);
   });
 
   it("Detail a user successfully", async () => {
@@ -72,15 +38,16 @@ describe("Detail User Controller Tests", () => {
 
   it("Jwt mal formed", async () => {
     let errorToken;
+
     await detailUser(errorToken);
 
     expect(response.status).toBe(401);
-
     expect(response.body).toHaveProperty("message", "jwt malformed");
   });
 
   it("Invalid signature", async () => {
-    let errorToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    const errorToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
     await detailUser(errorToken);
 
     expect(response.status).toBe(401);
@@ -89,19 +56,17 @@ describe("Detail User Controller Tests", () => {
   });
 
   it("Unauthorized", async () => {
-    await unauthUser();
+    response = await unauthUser("get", server, "user");
 
     expect(response.status).toBe(401);
-
     expect(response.body).toHaveProperty("message", "unauthorized");
   });
 
   it("User not found", async () => {
-    await deleteUser(bearerToken);
+    await deleteUser(bearerToken, user.password);
     await detailUser(bearerToken);
 
     expect(response.status).toBe(404);
-
     expect(response.body).toHaveProperty("message", "user not found");
   });
 });
