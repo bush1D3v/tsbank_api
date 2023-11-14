7b9ec76ea1a5b6ebaa691daef997253cea7a234d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-14 00:01:23 -0300

    refactor(validatePassword.ts): Melhorando nome do segundo parametro

--- src/utils/validatePassword.ts
+++ src/utils/validatePassword.ts
@@ -1,8 +1,8 @@
 import { compare } from "bcrypt";
 import { HttpStatusError } from "../error";
 
-export default async function validatePassword(reqPassword: string, userPassword: string) {
-  const password = await compare(reqPassword, userPassword);
+export default async function validatePassword(reqPassword: string, dataPassword: string) {
+  const password = await compare(reqPassword, dataPassword);
 
   if (!password) {
     throw new HttpStatusError("invalid password", 401);
