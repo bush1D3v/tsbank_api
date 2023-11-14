fbbda5f2f0c7321fc9e83dae25df7e61b444749e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:29:57 -0300

    update(getHistoryAndReturn.ts): Adiciona verificacao de existencia de transacoes

--- src/services/transaction/getHistoryAndReturn.ts
+++ src/services/transaction/getHistoryAndReturn.ts
@@ -1,11 +1,14 @@
 import { Request } from "express";
 import { getToken } from "../../utils";
+import { verifyAccountTransactions } from "../../providers";
 import { getTransactions, getTypedTransactions } from "../../repositories";
 
 export default async function getHistoryAndReturn(req: Request) {
   const id = getToken(req);
 
   const transactions = await getTransactions(id);
+
+  verifyAccountTransactions(transactions);
 
   const typedTransactions = getTypedTransactions(transactions);
 
