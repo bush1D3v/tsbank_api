d1c6c622d429a5a353ab392b2de9a88a0f7e1000
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 22:06:11 -0300

    build(jest.config.js): Removendo configuracao duplicada

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,6 @@ module.exports = {
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "js"],
   setupFilesAfterEnv: ["./tests/testsSetup.ts"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[tj]s$",
   transform: {
     "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.json" }],
