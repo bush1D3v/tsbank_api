49769f29067e9c0e94f53051d8e1666e770bda3d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:27:04 -0300

    update(GeneralCardSchema.ts): Adicionando tamanho minimo ao nome

--- src/schemas/user/GeneralCardSchema.ts
+++ src/schemas/user/GeneralCardSchema.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 const GeneralCardSchema = yup.object({
   card_number: yup.string().min(16).max(16).required(),
-  cardholder_name: yup.string().required(),
+  cardholder_name: yup.string().min(10).required(),
   expiration_date: yup.date().required(),
   cvv: yup.string().min(3).max(3).required(),
   password: yup.string().min(4).max(6).required(),
