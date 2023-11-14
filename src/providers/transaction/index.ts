37e3b252eaf469a7bab0e82ef6283f863f850e64
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:58:30 -0300

    update(index.ts): Atualiza exportacao de provedores relacionados a transacao

--- src/providers/transaction/index.ts
+++ src/providers/transaction/index.ts
@@ -1,11 +1,13 @@
 import validateOutput from "./validateOutput";
 import validatePix from "./validatePix";
 import validateTransaction from "./validateTransaction";
+import verifyAccountTransactions from "./verifyAccountTransactions";
 import verifyTransactionId from "./verifyTransactionId";
 
 export {
   verifyTransactionId,
   validateTransaction,
   validatePix,
-  validateOutput
+  validateOutput,
+  verifyAccountTransactions
 };
