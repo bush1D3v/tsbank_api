56db98dfa2b41d4dabec2acca2cf1fdc45164a21
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:15:46 -0300

    refactor(validateOutput.ts): Refatorando resposta de erro

--- src/providers/transaction/validateOutput.ts
+++ src/providers/transaction/validateOutput.ts
@@ -2,6 +2,6 @@ import { HttpStatusError } from "../../error";
 
 export default function validateOutput(actualBalance: number, outputValue: number) {
   if (actualBalance < outputValue) {
-    throw new HttpStatusError("Insufficient balance to perform the transaction", 400);
+    throw new HttpStatusError("insufficient balance to perform the transaction", 400);
   }
 };
