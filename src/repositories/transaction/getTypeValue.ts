7f2dd830e89e5d58c9b0c95bf02e5205985bf472
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-14 09:45:44 -0300

    refactor(getTypeValue.ts): Refatora identacao do codigo

--- src/repositories/transaction/getTypeValue.ts
+++ src/repositories/transaction/getTypeValue.ts
@@ -2,6 +2,7 @@ import { DatabaseTransactionParams } from "../../models";
 
 export default function getTypeValue(typedTransactions: DatabaseTransactionParams[]) {
   let finalValue = 0;
+
   for (let transaction of typedTransactions) {
     finalValue += transaction.value;
   }
