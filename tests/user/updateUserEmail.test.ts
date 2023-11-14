dab2cf5270610f9301f7afd63bbc859e8eb9db36
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:46:39 -0300

    test(updateUserEmail.test.ts): Cria todos os testes para a rota atualizar email do usuario

--- tests/user/updateUserEmail.test.ts
+++ tests/user/updateUserEmail.test.ts
@@ -1,45 +1,20 @@
 import request from "supertest";
 import server from "../../src/server";
-import { LoginUserParams, UserParams } from "../../src/models";
+import { loginUser, user } from "../models";
+import {
+  deleteUser,
+  insertUserAndLogin,
+  unauthUser
+} from "../functions";
 
 type UpdateEmailUserTestParams = {
   new_email: string | null;
   password: string | null;
 };
 
-const user: UserParams = {
-  name: "Victor Navarro",
-  cpf: "12345678931",
-  phone: "21123456789",
-  email: "victorjln@gmail.com",
-  password: "vtjln123"
-};
-
-const loginUser: LoginUserParams = {
-  email: "victorjln@gmail.com",
-  password: "vtjln123"
-};
-
 let updatedEmailUser: UpdateEmailUserTestParams;
 let bearerToken: string | undefined;
 let response: request.Response;
-
-const unauthUser = async () => {
-  response = await request(server)
-    .patch("/email")
-    .send(updatedEmailUser);
-
-  return response;
-};
-
-const deleteUser = async (token: string | undefined, password: string | null) => {
-  response = await request(server)
-    .delete("/user")
-    .set("Authorization", `Bearer ${token}`)
-    .send({ password });
-
-  return response;
-};
 
 const updateEmailUser = async (token: string | undefined, user: UpdateEmailUserTestParams) => {
   response = await request(server)
@@ -57,23 +32,14 @@ describe("Update User Email Controller Tests", () => {
       "password": "vtjln123"
     };
 
-    await request(server)
-      .post("/user")
-      .send(user);
-
-    const tokenReq = await request(server)
-      .post("/login")
-      .send(loginUser);
-
-    bearerToken = tokenReq.body.token;
+    bearerToken = await insertUserAndLogin(user, loginUser);
   });
 
   it("Update a email successfully", async () => {
     await updateEmailUser(bearerToken, updatedEmailUser);
 
     expect(response.status).toBe(201);
-
-    expect(response.body).toHaveProperty("message", `Your email has been changed successfully, now is '${updatedEmailUser.new_email}'`);
+    expect(response.body).toHaveProperty("message", `your email has been changed successfully, now is '${updatedEmailUser.new_email}'`);
   });
 
   it("Email must be a valid email", async () => {
@@ -114,27 +80,26 @@ describe("Update User Email Controller Tests", () => {
 
   it("Jwt mal formed", async () => {
     let errorToken;
+
     await updateEmailUser(errorToken, updatedEmailUser);
 
     expect(response.status).toBe(401);
-
     expect(response.body).toHaveProperty("message", "jwt malformed");
   });
 
   it("Invalid signature", async () => {
-    let errorToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    const errorToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
     await updateEmailUser(errorToken, updatedEmailUser);
 
     expect(response.status).toBe(401);
-
     expect(response.body).toHaveProperty("message", "invalid signature");
   });
 
   it("Unauthorized", async () => {
-    await unauthUser();
+    response = await unauthUser("patch", server, "email");
 
     expect(response.status).toBe(401);
-
     expect(response.body).toHaveProperty("message", "unauthorized");
   });
 
@@ -143,7 +108,6 @@ describe("Update User Email Controller Tests", () => {
     await updateEmailUser(bearerToken, updatedEmailUser);
 
     expect(response.status).toBe(404);
-
     expect(response.body).toHaveProperty("message", "user not found");
   });
 
