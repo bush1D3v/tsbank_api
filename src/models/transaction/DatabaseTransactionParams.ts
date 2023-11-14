06b033e4c05f2b5af9c03a70f3bf7d0fef3c41a8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:12:40 -0300

    fix(DatabaseTransactionParams.ts): Atualizando tipagem da key date

--- src/models/transaction/DatabaseTransactionParams.ts
+++ src/models/transaction/DatabaseTransactionParams.ts
@@ -3,5 +3,5 @@ import { TransactionParams } from "./TransactionParams";
 export interface DatabaseTransactionParams extends TransactionParams {
   id: number;
   user_id: number;
-  date: Date;
+  date: string;
 };
