22a1b3fb325e4cf8330184867b38b1f15f57fb40
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:50:05 -0300

    feat(UserModels.ts): Cria modelos de usuario para testes

---
+++ tests/models/user/UserModels.ts
@@ -0,0 +1,19 @@
+import { UserParams } from "../../../src/models";
+
+const user: UserParams = {
+  name: "Victor Navarro",
+  cpf: "12345678931",
+  phone: "21123456789",
+  email: "victorjln@gmail.com",
+  password: "vtjln123"
+};
+
+const user2: UserParams = {
+  name: "Victor José",
+  cpf: "12345678921",
+  phone: "31123456789",
+  email: "victorjln2@gmail.com",
+  password: "vtjln123"
+};
+
+export { user, user2 };
