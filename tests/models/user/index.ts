9d1ede8c7f3f08992c590571fbe8e69c7efe13c1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:50:47 -0300

    feat(index.ts): Cria index exportador de modelos dos usuarios

---
+++ tests/models/user/index.ts
@@ -0,0 +1,12 @@
+import { credit, debit } from "./CardModels";
+import { loginUser, loginUser2 } from "./LoginModels";
+import { user, user2 } from "./UserModels";
+
+export {
+  user,
+  user2,
+  loginUser,
+  loginUser2,
+  credit,
+  debit
+};
