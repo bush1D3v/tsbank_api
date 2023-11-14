0c69e59c581c73c6ba11c347afb8b4cc8a1add98
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:51:10 -0300

    feat(index.ts): Cria index exportador de modelos dos testes

---
+++ tests/models/index.ts
@@ -0,0 +1,23 @@
+import {
+  user,
+  user2,
+  loginUser,
+  loginUser2,
+  credit,
+  debit
+} from "./user";
+import {
+  deposit,
+  withdraw
+} from "./transaction";
+
+export {
+  user,
+  user2,
+  loginUser,
+  loginUser2,
+  credit,
+  debit,
+  deposit,
+  withdraw
+};
