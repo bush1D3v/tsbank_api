b205a74797384b2ca8696395ca70878a89a4e03b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-13 23:55:22 -0300

    feat(DepositModels.ts): Cria modelos de deposito para testes

---
+++ tests/models/transaction/DepositModels.ts
@@ -0,0 +1,9 @@
+import { DepositParams } from "../../../src/models";
+
+const deposit: DepositParams = {
+  "value": 30000,
+  "email": "victorjln@gmail.com",
+  "password": "vtjln123"
+};
+
+export default deposit;
