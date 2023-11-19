c044f0ed673d3e34219326a927d427515ba5587e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-18 23:39:44 -0300

    update(verifyCardBody.ts): Adiciona tipagem a constante card_type

--- src/middlewares/user/verifyCardBody.ts
+++ src/middlewares/user/verifyCardBody.ts
@@ -2,12 +2,13 @@ import { Request, Response, NextFunction } from "express";
 import { handleError } from "../../error";
 import { CardSchema } from "../../schemas";
 import { validateCardType } from "../../providers";
+import { CardType } from "../../models";
 
 export default async function verifyCardBody(req: Request, res: Response, next: NextFunction) {
   try {
     await CardSchema.validate(req.body);
 
-    const { card_type } = req.body;
+    const { card_type } = req.body as CardType;
 
     validateCardType(card_type);
 
