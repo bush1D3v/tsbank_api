66676e46d11c959eb62c75c9416fe0ddead3f616
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-20 14:03:15 -0300

    update(index.ts): Atualiza exportacao de rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,11 @@
 import userRoutes from "./userRoutes";
 import transactionRoutes from "./transactionRoutes";
 import swaggerRoutes from "./swaggerRoutes";
+import renderAutoReq from "./renderAutoReq";
 
-export { userRoutes, transactionRoutes, swaggerRoutes };
+export {
+  userRoutes,
+  transactionRoutes,
+  swaggerRoutes,
+  renderAutoReq
+};
