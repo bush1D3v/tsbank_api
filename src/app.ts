fa4b305f5d4882df6088302674e3da4a585221c9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-20 14:02:56 -0300

    update(app.ts): Adicionando rota para requisicao automatica ao render

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import { userRoutes, transactionRoutes, swaggerRoutes } from "./routes";
+import {
+  userRoutes,
+  transactionRoutes,
+  swaggerRoutes,
+  renderAutoReq
+} from "./routes";
 
 const app = express();
 
@@ -10,5 +15,6 @@ app.use(express.json());
 app.use(swaggerRoutes);
 app.use(userRoutes);
 app.use(transactionRoutes);
+app.use(renderAutoReq);
 
 export default app;
