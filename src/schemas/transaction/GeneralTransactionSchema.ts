89467384c3c273ad4d622564f5a8ee4992c29b1b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-08 17:42:35 -0300

    fix(GeneralTransactionSchema.ts): Removendo verificacao que nao aceitava valores entre 0.01 e 0.09

--- src/schemas/transaction/GeneralTransactionSchema.ts
+++ src/schemas/transaction/GeneralTransactionSchema.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const GeneralTransactionSchema = yup.object({
   type: yup.string().required(),
   description: yup.string().required(),
-  value: yup.number().min(0.1).required(),
+  value: yup.number().min(0.01).required(),
   password: yup.string().min(8).required(),
   email: yup.string().email().required(),
   cpf: yup.string().min(11).max(11).required()
