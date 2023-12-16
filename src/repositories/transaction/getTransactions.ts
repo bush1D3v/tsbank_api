2169bf5670580df8cbd27e112d68ead65a279532
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 00:13:28 -0300

    fix(getTransactions.ts): Adicionando formatacao de data as transacoes

--- src/repositories/transaction/getTransactions.ts
+++ src/repositories/transaction/getTransactions.ts
@@ -1,8 +1,15 @@
 import db from "../../data/connection";
 import { DatabaseTransactionParams } from "../../models";
+import { dateFormatter } from "../../utils";
 
 export default async function getTransactions(id: number) {
   const transactions: DatabaseTransactionParams[] = await db("transactions").where({ user_id: id });
+
+  if (transactions) {
+    for (let transaction of transactions) {
+      transaction.date = dateFormatter(transaction.date);
+    }
+  };
 
   return transactions;
 };
