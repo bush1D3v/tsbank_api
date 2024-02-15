84986856035580f705859b497990225be2040908
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-15 16:45:29 -0300

    update(GeneralUserSchema.ts): Atualiza limitacao de caracteres dos dados

--- src/schemas/user/GeneralUserSchema.ts
+++ src/schemas/user/GeneralUserSchema.ts
@@ -1,14 +1,14 @@
 import * as yup from "yup";
 
 const GeneralUserSchema = yup.object({
-  name: yup.string().required(),
-  email: yup.string().email().required(),
+  name: yup.string().max(75).required(),
+  email: yup.string().email().max(75).required(),
   cpf: yup.string().min(11).max(11).required(),
   phone: yup.string().min(10).max(11).required(),
   new_phone: yup.string().min(10).max(11).required(),
-  password: yup.string().min(8).required(),
-  new_password: yup.string().min(8).required(),
-  new_email: yup.string().email().required()
+  password: yup.string().min(8).max(16).required(),
+  new_password: yup.string().min(8).max(16).required(),
+  new_email: yup.string().email().max(75).required()
 });
 
 export default GeneralUserSchema;
