d58568ecda20af424764b7c1b4ac26951fa79016
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-15 16:46:27 -0300

    refactor(deleteTransactionAndConfirm.ts): Removendo validacao desnecessaria

--- src/services/transaction/deleteTransactionAndConfirm.ts
+++ src/services/transaction/deleteTransactionAndConfirm.ts
@@ -2,11 +2,7 @@ import { Request } from "express";
 import { dropTransaction, getUserPerId } from "../../repositories";
 import { DeleteTransactionParams } from "../../models";
 import { getToken, validatePassword } from "../../utils";
-import {
-  undefinedUser,
-  validateTransaction,
-  verifyTransactionId
-} from "../../providers";
+import { validateTransaction, verifyTransactionId } from "../../providers";
 
 export default async function deleteTransactionAndConfirm(req: Request, params: DeleteTransactionParams) {
   await verifyTransactionId(params.transaction_id);
@@ -14,8 +10,6 @@ export default async function deleteTransactionAndConfirm(req: Request, params: 
   const userId = getToken(req);
 
   const user = await getUserPerId(userId);
-
-  undefinedUser(user);
 
   await validatePassword(params.password, user.password);
 
