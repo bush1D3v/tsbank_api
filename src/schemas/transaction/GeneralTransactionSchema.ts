31a6417838aa83407c2453ea125f0fd1d89a9b98
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-15 16:45:08 -0300

    update(GeneralTransactionSchema.ts): Atualiza limitacao de caracteres dos dados

--- src/schemas/transaction/GeneralTransactionSchema.ts
+++ src/schemas/transaction/GeneralTransactionSchema.ts
@@ -1,11 +1,11 @@
 import * as yup from "yup";
 
 const GeneralTransactionSchema = yup.object({
-  type: yup.string().required(),
+  type: yup.string().min(5).max(6).required(),
   description: yup.string().required(),
   value: yup.number().min(0.01).required(),
-  password: yup.string().min(8).required(),
-  email: yup.string().email().required(),
+  password: yup.string().min(8).max(16).required(),
+  email: yup.string().email().max(75).required(),
   cpf: yup.string().min(11).max(11).required()
 });
 
