89324e9fc1b2bbc98eac92464f3dd125393f2e28
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-15 16:45:18 -0300

    update(GeneralCardSchema.ts): Atualiza limitacao de caracteres dos dados

--- src/schemas/user/GeneralCardSchema.ts
+++ src/schemas/user/GeneralCardSchema.ts
@@ -3,11 +3,11 @@ import * as yup from "yup";
 const GeneralCardSchema = yup.object({
   card_number: yup.string().min(16).max(16).required(),
   cardholder_name: yup.string().min(10).required(),
-  expiration_date: yup.date().required(),
+  expiration_date: yup.date().min(5).max(5).required(),
   cvv: yup.string().min(3).max(3).required(),
   password: yup.string().min(4).max(6).required(),
   new_password: yup.string().min(4).max(6).required(),
-  card_type: yup.string().required(),
+  card_type: yup.string().min(5).max(6).required(),
   value: yup.number().min(0.01).required()
 });
 
